601469554f9ebc9e7d98844add4dda5062abb033
mathias juergens <m.juerg@online.de>
2013-07-04 20:56:41 +0200

    -

--- AndroidFirst/src/test/de/androidfirst/MainActivity.java
+++ AndroidFirst/src/test/de/androidfirst/MainActivity.java
@@ -1,35 +1,45 @@
 package test.de.androidfirst;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
 
 public class MainActivity extends Activity {
+
+	int counter;
+
+	Button add, sub;
+	TextView display;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		counter = 0;
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.tvDisplay);
+
+		add.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				counter++;
+				
+				display.setText("your total is " + counter);
+			}
+		});
+
+		sub.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				counter--;
+				display.setText("your total is " + counter);
+			}
+		});
 	}
-
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
-//	}
-	
-	public void sendMessage(View view){
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-	    EditText editText = (EditText) findViewById(R.id.edit_message);
-	    String message = editText.getText().toString();
-	    intent.putExtra(EXTRA_MESSAGE, message);
-	    startActivity(intent);
-
-		
-	}
-
 }
